92f0a17b6b3e94ee39073c81099b59c24c16e04b
MrNaughtZero <neil.naught@icloud.com>
2020-12-11 00:19:14 +0000

    dependancies updated && classes updated

--- app/forms.py
+++ app/forms.py
@@ -1,16 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, RadioField, validators, FileField, TextAreaField, HiddenField, MultipleFileField, SelectField, BooleanField, IntegerField
-from wtforms.validators import InputRequired, Email, Length, NumberRange, EqualTo
+from wtforms import StringField, PasswordField, SubmitField, validators
+from wtforms.validators import InputRequired, Email, Length, EqualTo
 
-class UserRegister(FlaskForm):
-    profile_img = FileField(validators=[InputRequired()])
+class AdminRegister(FlaskForm):
     email = StringField(validators=[InputRequired(), Email('Invalid Email'), Length(max=300)])
     username = StringField(validators=[InputRequired(), Length(max=300)])
     password = PasswordField(validators=[InputRequired(), EqualTo('confirm_password')])
     confirm_password = PasswordField(validators=[InputRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
-class UserLogin(FlaskForm):
+class AdminLogin(FlaskForm):
     username = StringField(validators=[InputRequired(), Length(max=300)])
     password = PasswordField(validators=[InputRequired()])
     submit = SubmitField('Login')
