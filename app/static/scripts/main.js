59f90d54ac87fadd0ff512c79af8eb1ddce49e2a
MrNaughtZero <neil.naught@icloud.com>
2020-12-11 23:10:10 +0000

    function created to save text editor to request.content value

--- app/static/scripts/main.js
+++ app/static/scripts/main.js
@@ -1,3 +1,9 @@
 function loadTextEditor(id){
     CKEDITOR.replace(id);
 }
+
+function saveTextEditorValue(id){
+    var data = CKEDITOR.instances[id].getData();
+    console.log(data);
+    document.getElementById('content').value = data;
+}
