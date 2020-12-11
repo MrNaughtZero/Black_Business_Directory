47d1368ffb4ff245bac9cf185320254c8387078e
MrNaughtZero <neil.naught@icloud.com>
2020-12-11 00:01:41 +0000

    User table updated ~ Role, Login Attempts, PW_reset added. Generate PW Reset function created. Check if admin exists created

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from app.database import db
 import hashlib
 from hashlib import sha256
+from random import choice
+import string
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -8,9 +10,15 @@ class User(db.Model):
     username = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), nullable=False)
     password = db.Column(db.String(255), nullable=False)
+    role = db.Column(db.String(100), nullable=False)
+    login_attempts = db.Column(db.Integer, nullable=True)
+    pw_reset = db.Column(db.String(500), nullable=True)
     
     def get_id(self):
         return self.id
+
+    def get_role(self):
+        return self.role
     
     def is_authenticated(self):
         return True
@@ -34,3 +42,12 @@ class User(db.Model):
     @staticmethod
     def hash_password(password):
         return sha256(password.encode('utf-8')).hexdigest()
+
+    @staticmethod
+    def generate_password_reset(StringLength=200):
+        generate = string.ascii_letters + string.ascii_lowercase + string.ascii_uppercase
+        return ''.join(choice(generate) for i in range(StringLength))
+
+    def check_for_admin(self):
+        ''' check if admin exists. if so, return false '''
+        return self.query.filter_by(role='admin').first()
