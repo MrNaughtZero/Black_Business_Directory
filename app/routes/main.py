43e0863f180fd67afc0b4854a6830968eb1316f0
MrNaughtZero <neil.naught@icloud.com>
2020-12-16 22:07:34 +0000

    index route updated

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,7 +1,7 @@
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, render_template
 
 main_bp = Blueprint("main_bp", __name__)
 
 @main_bp.route('/', methods=['GET'])
 def index():
-    return 'index'
+    return render_template('/main/index.html')
