32d6a6ec54f5d85f5f1b3894cd59613b1a8c5720
MrNaughtZero <neil.naught@icloud.com>
2020-12-16 22:08:14 +0000

    functions for admin navigation created

--- app/static/scripts/main.js
+++ app/static/scripts/main.js
@@ -1,3 +1,5 @@
+// Text Editor for Posts
+
 function loadTextEditor(id){
     CKEDITOR.replace(id);
 }
@@ -6,4 +8,21 @@ function saveTextEditorValue(id){
     var data = CKEDITOR.instances[id].getData();
     console.log(data);
     document.getElementById('content').value = data;
+}
+
+function preloadTextEditor(id, content){
+    document.getElementById(id).value = content;
+}
+
+// Admin side menu
+
+function menuShowSubPosts(){
+    document.getElementsByClassName('hidden-post-links')[0].style.display = 'block';
+    document.getElementById('showPostsLink').onclick = menuHideSubPosts;
+    
+}
+
+function menuHideSubPosts(){
+    document.getElementsByClassName('hidden-post-links')[0].style.display = 'none';
+    document.getElementById('showPostsLink').onclick = menuShowSubPosts;
 }
